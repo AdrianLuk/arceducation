6c95dd4999662c2391003364bf2907cec3248458
Adrian Luk <aluk@elitedigital.ca>
2018-08-19 15:00:51 -0400

    initial commit

---
+++ functions.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Understrap functions and definitions
+ *
+ * @package understrap
+ */
+
+/**
+ * Initialize theme default settings
+ */
+require get_template_directory() . '/inc/theme-settings.php';
+
+/**
+ * Theme setup and custom theme supports.
+ */
+require get_template_directory() . '/inc/setup.php';
+
+/**
+ * Register widget area.
+ */
+require get_template_directory() . '/inc/widgets.php';
+
+/**
+ * Enqueue scripts and styles.
+ */
+require get_template_directory() . '/inc/enqueue.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Custom pagination for this theme.
+ */
+require get_template_directory() . '/inc/pagination.php';
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/inc/extras.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Custom Comments file.
+ */
+require get_template_directory() . '/inc/custom-comments.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Load custom WordPress nav walker.
+ */
+require get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
+
+/**
+ * Load WooCommerce functions.
+ */
+require get_template_directory() . '/inc/woocommerce.php';
+
+/**
+ * Load Editor functions.
+ */
+require get_template_directory() . '/inc/editor.php';
+add_action( 'init', function(){
+    new cpt_register;
+} );
+class cpt_register {
+    public function __construct(){
+        $this->cpt_register('Homeschool Program', 'homeschool', 'dashicons-admin-home', array());
+        $this->cpt_register('Camp', 'camp', 'dashicons-palmtree', array());
+        $this->cpt_register('Classe', 'class', 'dashicons-welcome-learn-more', array());
+		$this->cpt_register('Staff', 'staff', 'dashicons-groups', array());
+    }
+
+    public function cpt_register($cpt, $cpt_name, $icon = 'dashicons-businessman', $supports = array()){
+        $labels = array(
+            'name'               => _x( $cpt . 's', $cpt . 's', 'Schoolhouse' ),
+            'singular_name'      => _x( $cpt, $cpt, 'Schoolhouse' ),
+            'menu_name'          => _x( $cpt . 's', 'admin menu', 'Schoolhouse' ),
+            'name_admin_bar'     => _x( $cpt, 'add new on admin bar', 'Schoolhouse' ),
+            'add_new'            => _x( 'Add New', $cpt, 'Schoolhouse' ),
+            'add_new_item'       => __( 'Add New ' . $cpt, 'Schoolhouse' ),
+            'new_item'           => __( 'New '. $cpt, 'Schoolhouse' ),
+            'edit_item'          => __( 'Edit '.$cpt, 'Schoolhouse' ),
+            'view_item'          => __( 'View '.$cpt, 'Schoolhouse' ),
+            'all_items'          => __( 'All ' . $cpt . 's', 'Schoolhouse' ),
+            'search_items'       => __( 'Search ' .$cpt . 's', 'Schoolhouse' ),
+            'parent_item_colon'  => __( 'Parent ' .$cpt . 's:', 'Schoolhouse' ),
+            'not_found'          => __( 'No ' .$cpt . 's found.', 'Schoolhouse' ),
+            'not_found_in_trash' => __( 'No ' .$cpt . 's found in Trash.', 'Provisus' )
+        );
+
+        $args = array(
+            'labels'              => $labels,
+            'hierarchical'        => false,
+            'description'         => __( 'You can add '.$cpt.' here!.', 'Provisus' ),
+            'taxonomies'          => array(),
+            'public'              => true,
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'show_in_admin_bar'   => true,
+            'menu_position'       => null,
+            'menu_icon'           => $icon,
+            'show_in_nav_menus'   => true,
+            'publicly_queryable'  => true,
+            'exclude_from_search' => false,
+            'has_archive'         => true,
+            'query_var'           => true,
+            'can_export'          => true,
+            'rewrite'             => true,
+            'capability_type'     => 'post',
+            'supports'            => $supports 
+        );
+        register_post_type($cpt_name,$args);
+    }
+}
