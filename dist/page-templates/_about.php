59c3c596827195e1410e88d7f9b2cf8746cacb89
Adrian <lukabaseball@hotmail.com>
2019-04-01 00:24:06 -0400

    stuff

--- dist/page-templates/_about.php
+++ dist/page-templates/_about.php
@@ -12,7 +12,7 @@ $container = 'container';
 <div class="wrapper" id="about-wrapper">
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
         <div class="row">
-            <main class="site-main mx-auto" id="main">
+            <main class="site-main mx-auto container" id="main">
                 <header class="page-header">
                     <h1 class="page-title text-center"><?php the_title(); ?></h1>
                 </header><!-- .page-header -->
